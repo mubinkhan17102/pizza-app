1b644b76231c6767ca726c693a6f7b8c3e0325d6
mubin <mobin17102@gmail.com>
2020-12-28 19:55:43 +0600

    end front end

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const path = require('path');
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 
-app.get('/', (req, res) => {
-    res.render('test');
-})
-
+app.use(express.static('public'));
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resource/views'));
 app.set('view engine', 'ejs');
-
+require('./route/web')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server starts on port ${PORT}`);
